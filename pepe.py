2b74816a71ec235148f9bd44c23b3d7b3c0a80f4
Crissper <Cris.per.146@gmail.com>
2021-10-14 17:15:43 -0300

    first commit

---
+++ pepe.py
@@ -0,0 +1,16 @@
+
+from os import replace
+
+
+filename = 'links.txt'
+
+with open(filename) as f_obj:
+    prueba = f_obj.read()
+
+print(prueba)
+
+qw = prueba.replace('#' , '!') .replace('file/','#!')
+
+
+
+print(qw)
