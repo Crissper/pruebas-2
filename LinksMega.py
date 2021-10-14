53ff21753bf5a685b00f09e38eb5a91e97321a31
Niko <acosta.nicolas@hotmail.es>
2021-10-14 19:43:43 -0300

    Prueba1Nico

--- LinksMega.py
+++ LinksMega.py
@@ -9,5 +9,10 @@ with open(filename) as f_obj:
 print(prueba)
 
 qw = prueba.replace('#' , '!') .replace('file/','#!')
+wq=qw 
+""" Este es un comentario multilinea. La
+siguiente parte realiza una serie
+de cosas muy chulas  EL NIKOO"""
+"""asdas"""
 
 print(qw)
